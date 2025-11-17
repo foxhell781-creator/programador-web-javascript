e21b6efe0307c6b89035960d1020e8a63b80bb4c
foxhell781-creator <foxhell781@gmail.com>
2025-11-17 11:53:50 -0300

    Add files via upload

---
+++ java script2/laços.js
@@ -0,0 +1,19 @@
+console.log("Ola mundo");
+console.log("Ola mundo");
+console.log("Ola mundo");
+console.log("Ola mundo");
+console.log("Ola mundo");
+
+//Criamos uma variavel for menor que 5, o bloco dentro do while será executado
+while (numeroDeVezes < 5) {
+
+    console.log("Esse é um exemplo de loop while! Rodada número: " + numeroDeVezes);
+
+    //a cada repetição, somamos 1 à variavel para evitar um loop infinito
+    numeroDeVezes = numeroDeVezes + 1;
+}
+
+for (let i=1;i<=10;i++){
+    console.log("contador for:",i);
+}
+
---
+++ java script2/script.js
@@ -0,0 +1,11 @@
+/*Complete o codigo para: Adicionar "limão" no final Remover o primeiro  elemento Ordenar alfabeticamente Inverter a ordem*/
+
+let mercado = ["uva", "maçã", "banana"];
+
+/*Qual o valor valor final de array cores após as seguintes operações*/
+
+let cores = ["azul", "verde"];
+cores.push("amarelo");
+cores.unshift("vermelho");
+cores.pop();
+cores.shift();
